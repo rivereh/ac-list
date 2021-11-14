3481a65947012f169f482f7ea57dc0228a2e5346
River <riverehill@gmail.com>
2021-11-13 23:42:52 -0800

    Fixed bugs

--- main.js
+++ main.js
@@ -2,14 +2,21 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!',
+        month: 11,
         fishes: null
     },
     methods: {
         getFishData: function() {
             fetch("./data/fish.json")
             .then(response => response.json())
-            .then(data => (this.fishes = data));
+            .then(data => (this.fishes = data))
+        },
+        titleCase: function(str) {
+            var splitStr = str.toLowerCase().split(' ')
+            for (var i = 0; i < splitStr.length; i++) {
+                splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1)
+            }
+            return splitStr.join(' ')
         }
     },
     mounted: function() {
